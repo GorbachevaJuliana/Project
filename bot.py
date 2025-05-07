42fdc759c53265a527ea90e769c2c24ccaaed1ad
Юлиана <gorbacheva.juliana@gmail.com>
2025-05-07 16:11:20 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,19 @@
 import asyncio
 import os
 import random
+import sqlite3
 from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ParseMode
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 from aiogram.client.default import DefaultBotProperties
-API_TOKEN = ''
+from dotenv import load_dotenv
+API_TOKEN = '8003685426:AAGbaa-jzCyNoHPCLA-kZE8Ilq3WrVPyb5o'
+
+conn = sqlite3.connect('project_db.db')
+cursor = conn.cursor()
+
 class Form(StatesGroup):
     name = State()
     rating = State()
@@ -21,38 +27,53 @@ class Form(StatesGroup):
 #     resize_keyboard=True,
 #     one_time_keyboard=True
 # )
-async def cmd_start(message,state):
+async def cmd_start(message: Message, state: FSMContext):
     await state.clear()
     answers = ['Привет, как зовут игрока?', 'Введи имя игрока']
     await message.answer(random.choice(answers))
     await state.set_state(Form.name)
 
-async def name_chose(message, state):
+async def name_chose(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
     await message.answer('Введи рейтинг игрока')
     await state.set_state(Form.rating)
 
-async def rating_chose(message, state):
+async def rating_chose(message: Message, state: FSMContext):
     await state.update_data(rating=message.text)
     await message.answer('Сколько ему лет?')
     await state.set_state(Form.age)
 
-async def age_chose(message, state):
+async def age_chose(message: Message, state: FSMContext):
     await state.update_data(age=message.text)
     await message.answer('Из какой он страны?')
     await state.set_state(Form.country)
 
-async def country_chose(message, state):
-    await state.update_date(country=message.text)
-    
+async def country_chose(message: Message, state: FSMContext):
+    await state.update_data(country=message.text)
+    data = await state.get_data()
+    await message.answer(f"Твои данные {data}")
+    cursor.executemany('''
+        INSERT INTO player (name, rating, age, country)
+        VALUES (?, ?, ?, ?)
+        ''', [
+            (data['name'],
+            data['rating'],
+            data['age'],
+            data['country'])
+        ])
+    conn.commit()
+    await state.clear()
 
 
 async def main():
     bot = Bot(token=API_TOKEN)
     dp = Dispatcher()
     dp.message.register(cmd_start, F.text == '/start')
-    dp.message.register(gender_chose, Form.gender)
     dp.message.register(name_chose, Form.name)
+    dp.message.register(rating_chose, Form.rating)
+    dp.message.register(age_chose, Form.age)
+    dp.message.register(country_chose, Form.country)
     await dp.start_polling(bot)
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
